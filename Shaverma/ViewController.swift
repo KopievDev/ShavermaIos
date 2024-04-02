13941c7cec93e73dc7a2f9e19a40b5a9360b6a4b
Kopiev Ivan <x@mustdev.ru>
2024-04-02 07:52:05 +0300

    Initial Commit

---
+++ Shaverma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shaverma
+//
+//  Created by Иван Копиев on 02.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
