a3278a894219e406480eb9886656413692417f55
Kopiev Ivan <x@mustdev.ru>
2024-04-03 11:31:32 +0300

    add create products request

--- Sources/App/Controllers/ProductsController.swift
+++ Sources/App/Controllers/ProductsController.swift
@@ -8,14 +8,15 @@
 import Foundation
 import Vapor
 import VaporToOpenAPI
+import Fluent
 
 struct ProductsController: RouteCollection {
 
     let tokenProtected: RoutesBuilder
 
     func boot(routes: RoutesBuilder) throws {
-        let users = tokenProtected.grouped("categories")
-        users.get(use: categories)
+        let category = tokenProtected.grouped("categories")
+        category.get(use: categories)
             .openAPI(
                 tags: .init(name: "Products"),
                 summary: "Категории",
@@ -27,10 +28,85 @@ struct ProductsController: RouteCollection {
                 responseDescription: "Success response"
             )
 
-        func categories(req: Request) async throws -> [CategoriesResponse] {
-            _ = try req.auth.require(User.self)
-            let categories = try await Categoties.query(on: req.db).all()
-            return categories.map { CategoriesResponse(id: $0.id ?? .generateRandom(), name: $0.name) }
+        let productRoute = tokenProtected.grouped("products")
+        productRoute.get(use: products)
+            .openAPI(
+                tags: .init(name: "Products"),
+                summary: "Список всех продуктов",
+                description: "Список всех доступных продуктов",
+                headers: .all(of: .type(Headers.AccessToken.self)),
+                contentType: .application(.json),
+                response: .type([ProductsResponse].self),
+                responseContentType: .application(.json),
+                responseDescription: "Success response"
+            )
+
+        productRoute.post(use: createProduct)
+            .openAPI(
+                tags: .init(name: "Products"),
+                summary: "Добавить продукт",
+                description: "Добавление продукта в БД",
+                headers: .all(of: .type(Headers.AccessToken.self)),
+                body: .type(ProductsRequest.self),
+                contentType: .application(.json),
+                response: .type(ProductsResponse.self),
+                responseContentType: .application(.json),
+                responseDescription: "Success response"
+            )
+    }
+
+    func categories(req: Request) async throws -> [CategoriesResponse] {
+        _ = try req.auth.require(User.self)
+        let categories = try await Categoties.query(on: req.db).all()
+        return categories.map { CategoriesResponse(id: $0.id ?? .generateRandom(), name: $0.name) }
+    }
+
+    func products(req: Request) async throws -> [ProductsResponse] {
+        _ = try req.auth.require(User.self)
+        let products = try await Products.query(on: req.db).all()
+        return products.map {
+            ProductsResponse(
+                id: $0.id ?? .generateRandom(),
+                name: $0.name,
+                desc: $0.desc,
+                price: $0.price,
+                category: $0.$category.id
+            )
         }
     }
+
+    func createProduct(req: Request) async throws -> ProductsResponse {
+        let user = try req.auth.require(User.self)
+        guard user.role == .admin else {
+            throw Abort(.forbidden)
+        }
+        let productRequest = try req.content.decode(ProductsRequest.self)
+
+        let category = try await Categoties
+            .query(on: req.db)
+            .filter(\.$id == productRequest.category)
+            .first()
+        guard let category else {
+            throw Abort(.custom(code: 404, reasonPhrase: "Нет такой категории"))
+        }
+
+        let product = try Products(
+            id: .generateRandom(),
+            name: productRequest.name,
+            desc: productRequest.desc,
+            price: productRequest.price,
+            categoryID: category.requireID()
+        )
+
+        try await product.save(on: req.db)
+
+        return try ProductsResponse(
+            id: product.requireID(),
+            name: product.name,
+            desc: product.desc,
+            price: product.price,
+            category: category.requireID()
+        )
+    }
+
 }
---
+++ Sources/App/Models/DTO/ProductsResponse.swift
@@ -0,0 +1,54 @@
+//
+//  ProductsResponse.swift
+//  
+//
+//  Created by Иван Копиев on 03.04.2024.
+//
+
+import Vapor
+import Foundation
+import SwiftOpenAPI
+import VaporToOpenAPI
+
+@OpenAPIDescriptable()
+/// Продукты
+struct ProductsResponse: Content, WithExample {
+    /// id
+    let id: UUID
+    /// Название продукта
+    let name: String
+    /// Описание продукта
+    let desc: String
+    /// Стоимость в копейках
+    let price: Decimal
+    /// Категория продукта
+    let category: UUID
+
+    static var example: ProductsResponse = .init(
+        id: .generateRandom(),
+        name: "Шаурма",
+        desc: "Из котят",
+        price: 19900,
+        category: .generateRandom()
+    )
+}
+
+@OpenAPIDescriptable()
+/// Создание продукта
+struct ProductsRequest: Content, WithExample {
+    /// Название продукта
+    let name: String
+    /// Описание продукта
+    let desc: String
+    /// Стоимость в копейках
+    let price: Decimal
+    /// Категория продукта
+    let category: UUID
+
+    static var example: ProductsRequest = .init(
+        name: "Шаурма",
+        desc: "Из котят",
+        price: 19900,
+        category: .generateRandom()
+    )
+}
